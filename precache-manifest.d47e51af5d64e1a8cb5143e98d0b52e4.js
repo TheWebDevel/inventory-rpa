715406389950f5c353321f86818b0e3dc332540e
Sathish <sathish@skcript.com>
2019-08-28 17:07:06 +0530

    Updates

---
+++ precache-manifest.d47e51af5d64e1a8cb5143e98d0b52e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8354a24e02671f232b0a3cefe33c2990",
+    "url": "/inventory-rpa/index.html"
+  },
+  {
+    "revision": "2988ed4992c0e5eb28b2",
+    "url": "/inventory-rpa/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2fade69e50cb82292e7f",
+    "url": "/inventory-rpa/static/css/main.7c10070d.chunk.css"
+  },
+  {
+    "revision": "2988ed4992c0e5eb28b2",
+    "url": "/inventory-rpa/static/js/2.bd6e673b.chunk.js"
+  },
+  {
+    "revision": "2fade69e50cb82292e7f",
+    "url": "/inventory-rpa/static/js/main.59732997.chunk.js"
+  },
+  {
+    "revision": "b362cba22dd8d40ae5d8",
+    "url": "/inventory-rpa/static/js/runtime~main.781b8ca2.js"
+  }
+]);
